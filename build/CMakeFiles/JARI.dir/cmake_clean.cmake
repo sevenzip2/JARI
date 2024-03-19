6a742492f09eba7467c9538d833ceeff82837c44
sevenzip <seven7ziper@gmail.com>
2024-03-19 10:40:30 +0900

    MY FIRST STARTUP

---
+++ build/CMakeFiles/JARI.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/JARI.dir/main.cpp.o"
+  "CMakeFiles/JARI.dir/main.cpp.o.d"
+  "JARI"
+  "JARI.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/JARI.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/JARI.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for JARI.
+# This may be replaced when dependencies are built.

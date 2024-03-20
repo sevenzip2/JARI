0b6b5d8a7406e6c068d5b0dff1d184b264e2319d
SEVENZIP <seven7ziper@gmail.com>
2024-03-20 17:25:27 +0900

    gitignore 정리

--- build/random/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/user/JARI/random
-# Build directory: /home/user/JARI/build/random
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("test")
